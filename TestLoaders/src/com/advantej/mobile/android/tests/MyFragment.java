82d9ffde6d6357d1a4d7d506582899a39e3b0d6d
Tejas Lagvankar <tlagvankar@us145971.local>
2012-07-24 18:05:50 -0700

    Custom loader to read from a DB with callbacks from DB on changes.

--- TestLoaders/src/com/advantej/mobile/android/tests/MyFragment.java
+++ TestLoaders/src/com/advantej/mobile/android/tests/MyFragment.java
@@ -1,6 +1,10 @@
 package com.advantej.mobile.android.tests;
 
-import android.os.*;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
@@ -18,7 +22,7 @@ import com.example.R;
  * Date: 7/2/12
  * Time: 1:12 PM
  */
-public class MyFragment extends Fragment implements LoaderManager.LoaderCallbacks<Object>{
+public class MyFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = "MyFragment";
     private LoaderManager mLoaderManager;
@@ -30,13 +34,32 @@ public class MyFragment extends Fragment implements LoaderManager.LoaderCallback
     private TextView mTxtSomeTxt;
     private Button mRefresh;
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Log.d(TAG, "onActivityCreated, myTid : " + android.os.Process.myTid());
 
         mLoaderManager = getLoaderManager();
-        mLoaderManager.initLoader(MY_LOADER, null, this);
+        Loader loader = mLoaderManager.getLoader(MY_LOADER);
+        if (loader != null) {
+            if(!loader.isStarted()){
+                Log.d(TAG, "Loader is not null and not started");
+                mLoaderManager.initLoader(MY_LOADER, null, this);
+            } else {
+                Log.d(TAG, "Loader is not null and is started");
+            }
+        } else {
+            Log.d(TAG, "Loader is null");
+            mLoaderManager.initLoader(MY_LOADER, null, this);
+        }
+
     }
 
     @Override
@@ -53,51 +76,67 @@ public class MyFragment extends Fragment implements LoaderManager.LoaderCallback
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
         View fragmentView = inflater.inflate(R.layout.fragment_myfrag, container);
         mTxtSomeTxt = (TextView) fragmentView.findViewById(R.id.txt_someTxt);
         mRefresh = (Button) fragmentView.findViewById(R.id.btn_refresh);
-        mRefresh.setOnClickListener(new View.OnClickListener(){
+        mRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mLoaderManager.restartLoader(MY_LOADER, null, MyFragment.this);
+                //mLoaderManager.restartLoader(MY_LOADER, null, MyFragment.this);
+                //Put test data
+                MyDB.getDataBaseManager(getActivity()).delete(MyDB.T_TABLE, null, null);
+                ContentValues cv = new ContentValues();
+                cv.put("mystring", "hello " + SystemClock.currentThreadTimeMillis());
+                MyDB.getDataBaseManager(getActivity()).insert(MyDB.T_TABLE, null, cv);
             }
         });
         return fragmentView;
     }
 
     @Override
-    public Loader<Object> onCreateLoader(int i, Bundle bundle) {
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         Log.d(TAG, "onCreateLoader, myTid : " + android.os.Process.myTid());
-        return new MyLoader(getActivity());
+        return new MyLoader(getActivity().getApplicationContext());
     }
 
     @Override
-    public void onLoadFinished(Loader<Object> loader, Object data) {
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
         Log.d(TAG, "onLoadFinished, myTid : " + android.os.Process.myTid());
 
-        final String val = (String)data;
 
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                if(mTxtSomeTxt != null){
-                    mTxtSomeTxt.setText(val);
+        if(cursor.moveToFirst()){
+            //TODO : Run over the cursor to do something with the whole data (later ad a list + adapter)
+            final String val =  cursor.getString(cursor.getColumnIndex(MyDB.MyDBColumns.MYSTRING));
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (mTxtSomeTxt != null) {
+                        mTxtSomeTxt.setText(val);
+                    }
                 }
-            }
-        });
+            });
+        }
+
     }
 
     @Override
-    public void onLoaderReset(Loader<Object> objectLoader) {
+    public void onLoaderReset(Loader<Cursor> objectLoader) {
         Log.d(TAG, "onLoaderReset, myTid : " + android.os.Process.myTid());
 
         mHandler.post(new Runnable() {
             @Override
             public void run() {
-                if(mTxtSomeTxt != null){
+                if (mTxtSomeTxt != null) {
                     mTxtSomeTxt.setText("");
                 }
             }
         });
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
     }
 }
--- TestLoaders/src/com/advantej/mobile/android/tests/MyLoader.java
+++ TestLoaders/src/com/advantej/mobile/android/tests/MyLoader.java
@@ -1,7 +1,7 @@
 package com.advantej.mobile.android.tests;
 
 import android.content.Context;
-import android.os.*;
+import android.database.Cursor;
 import android.support.v4.content.AsyncTaskLoader;
 import android.util.Log;
 
@@ -11,48 +11,96 @@ import android.util.Log;
  * Date: 7/2/12
  * Time: 1:20 PM
  */
-public class MyLoader extends AsyncTaskLoader<Object> {
+public class MyLoader extends AsyncTaskLoader<Cursor> {
     private static final String TAG = "MyLoader";
+
+    //TODO Constructor to accept tablename and query parameters
+    private Cursor mCursor;
+    private final ForceLoadContentObserver mObserver;
+    private MyDB myDB;
 
     public MyLoader(Context context) {
         super(context);
+        mObserver = new ForceLoadContentObserver();
+        myDB = MyDB.getDataBaseManager(getContext()); // Is this safe ? or getApplicationContext is needed ?
     }
 
 
     //Runs on a worker thread
     @Override
-    public Object loadInBackground() {
+    public Cursor loadInBackground() {
         Log.d(TAG, "loadInBackground, myTid : " + android.os.Process.myTid());
+        Log.d(TAG, "loadInBackground, ObjectId : " + System.identityHashCode(MyLoader.this));
         //Do some fancy stuff and return an Object as the result !
         // After return, onLoadFinished() of MyFragment should be called.
-        SystemClock.sleep(1000 * 10);
-        String abc = "abc " + SystemClock.currentThreadTimeMillis();
-        return (Object)abc;
+        Cursor cursor = myDB.query(MyDB.T_TABLE, null, null, null, null, null, null, null);
+        if(cursor != null){
+            cursor.getCount();
+            cursor.registerContentObserver(mObserver);
+        }
+
+        return cursor;
+    }
+
+    @Override
+    public void deliverResult(Cursor cursorToDeliver) {
+        if(isReset()){
+           if(cursorToDeliver != null) {
+              cursorToDeliver.close();
+           }
+           return;
+        }
+
+        Cursor oldCursor = mCursor;
+        mCursor = cursorToDeliver;
+
+        if(isStarted()){
+            super.deliverResult(cursorToDeliver);
+        }
+
+        if (oldCursor != null && oldCursor != cursorToDeliver && !oldCursor.isClosed()) {
+            oldCursor.close();
+        }
     }
 
     @Override
     protected void onStartLoading() {
         Log.d(TAG, "onStartLoading, myTid : " + android.os.Process.myTid());
-        /* Follow this pattern
-        if(dataIsReady) {
-            deliverResult(data);
-          } else {
+        if(mCursor != null){
+           deliverResult(mCursor);
+        }
+
+        if(mCursor == null || takeContentChanged()) {
             forceLoad();
         }
-        */
-        forceLoad();
     }
 
     @Override
     protected void onStopLoading() {
         Log.d(TAG, "onStopLoading, myTid : " + android.os.Process.myTid());
-        super.onStopLoading();
+        cancelLoad();
+    }
+
+    @Override
+    public void onCanceled(Cursor cursor) {
+        if(cursor != null && !cursor.isClosed()){
+            cursor.close();
+        }
     }
 
     @Override
     protected void onReset() {
         Log.d(TAG, "onReset, myTid : " + android.os.Process.myTid());
         super.onReset();
+
+        stopLoading();
+
+        if(mCursor!= null && !mCursor.isClosed()){
+            mCursor.close();
+        }
+
+        mCursor = null;
+
     }
 
     @Override
@@ -60,5 +108,12 @@ public class MyLoader extends AsyncTaskLoader<Object> {
         Log.d(TAG, "onForceLoad, myTid : " + android.os.Process.myTid());
         super.onForceLoad();
     }
+
+    @Override
+    public void onContentChanged() {
+        Log.d(TAG, "onContentChanged, myTid : " + android.os.Process.myTid());
+        super.onContentChanged();
+    }
+
 }
 
